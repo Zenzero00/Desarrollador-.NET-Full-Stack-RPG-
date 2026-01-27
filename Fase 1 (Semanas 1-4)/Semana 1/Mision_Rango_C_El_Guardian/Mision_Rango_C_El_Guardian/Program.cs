22aef13c42625a8d081b4dcf7cc3c0e1865e0a2b
Francisco Unda <unda2001@gmail.com>
2026-01-27 19:24:16 -0400

    Tercera mision

---
+++ Fase 1 (Semanas 1-4)/Semana 1/Mision_Rango_C_El_Guardian/Mision_Rango_C_El_Guardian/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Mision_Rango_C_El_Guardian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nivelJugador = 3;
+            int nivelRequerido = 10;
+
+            if (nivelJugador >= nivelRequerido)
+            {
+                Console.WriteLine("Bienvenido, pasad.");
+            }
+            else
+            {
+                Console.WriteLine($"Alto ahí. Necesitas nivel {nivelRequerido} para entrar.");
+            }
+        }
+    }
+}
