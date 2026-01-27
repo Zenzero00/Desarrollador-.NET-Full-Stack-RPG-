50ed8df9ecb52a26804b872da533a77f4f85480f
Francisco Unda <unda2001@gmail.com>
2026-01-27 18:14:03 -0400

    Segunda mision

---
+++ Fase 1 (Semanas 1-4)/Semana 1/Mision_Rango_D_Logica_Booleana/Mision_Rango_D_Logica_Booleana/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mision_Rango_D_Logica_Booleana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tieneMana = true;
+            bool conoceHechizo = true;
+
+            bool puedeAtacar = tieneMana && conoceHechizo;
+
+            Console.WriteLine($"¿Zenzero puede atacar?: {puedeAtacar}");
+        }
+    }
+}
