bd23eeb6d14e60baf13e8ec94683d1f3e1cf09bd
Francisco Unda <unda2001@gmail.com>
2026-01-27 13:55:50 -0400

    Primera Mision

---
+++ Dia1_Inicio/Dia1_Inicio/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dia1_Inicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hola, Futuro Senior .NET");
+        }
+    }
+}
---
+++ Fase 1 (Semanas 1-4)/Semana 1/Mision_Rango_E/Mision_Rango_E/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mision_Rango_E_El_Despertar_de_la_Identidad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string identidad = "Zenzero";
+            int Nivel = 1;
+
+            Console.WriteLine("El Jugador " + identidad + " ha despertado con el Nivel de Poder: " + Nivel);
+        }
+    };
+};
