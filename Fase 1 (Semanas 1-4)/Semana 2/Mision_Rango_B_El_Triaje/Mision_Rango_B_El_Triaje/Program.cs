293cc61ffc3f057773fdaf0c95a63d17636ed2c3
Francisco Unda <unda2001@gmail.com>
2026-01-27 19:38:44 -0400

    Cuarta mision

---
+++ Fase 1 (Semanas 1-4)/Semana 2/Mision_Rango_B_El_Triaje/Mision_Rango_B_El_Triaje/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Mision_Rango_B_El_Triaje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vida = 40;
+
+            if (vida == 100)
+            {
+                Console.WriteLine("Estado: Perfecto.");
+            }
+            else if (vida >= 50)
+            {
+                Console.WriteLine("Estado: Herido, pero puedo luchar.");
+            }
+            else if (vida > 0)
+            {
+                Console.WriteLine("Estado: ¡PELIGRO CRÍTICO! Huye.");
+            }
+            else
+            {
+                Console.WriteLine("Estado: Muerto.");
+            }
+        }
+    }
+}
