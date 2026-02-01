24cde4d72ef1ea534cec34d49c08d6299c5e23b2
Francisco Unda <unda2001@gmail.com>
2026-02-01 16:08:49 -0400

    Novena Mision

---
+++ Fase 1 (Semanas 1-4)/Semana 2/Mision_Rango_C_Plus_Inventario/Mision_Rango_C_Plus_Inventario/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mision_Rango_C_Plus_Inventario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inventario = { "Poción" , "Espada" , "Mapa" , "Grimorio"};
+
+            foreach (string item in inventario)
+            {
+                Console.WriteLine($"Verificando equipo... Tienes: {item}");
+            }
+        }
+    }
+}
