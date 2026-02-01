47872665d0e1276523c15d5f871f2016b3f42540
Francisco Unda <unda2001@gmail.com>
2026-01-31 21:51:02 -0400

    Septima Mision

---
+++ Fase 1 (Semanas 1-4)/Semana 2/Mision_DoWhile_Fe/Mision_DoWhile_Fe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Mision_DoWhile_Fe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int objetosSagrados = 0;
+
+            do
+            {
+                Console.WriteLine("¡Intentado revivir con magia antigua!");
+            }
+            while (objetosSagrados > 0);
+        } 
+    }
+}
