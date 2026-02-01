25145f9fdc3feec550a055e495b9a05949cc3824
Francisco Unda <unda2001@gmail.com>
2026-01-31 21:13:46 -0400

    Sexta Mision

---
+++ Fase 1 (Semanas 1-4)/Semana 2/Mision_Bucle_Voluntad/Mision_Bucle_Voluntad/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Mision_Bucle_Voluntad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int energia = 5;
+
+            while (energia > 0)
+            {
+                Console.WriteLine($"Realizando golpe... Energia restante: {energia}");
+                energia--;
+            }
+            Console.WriteLine("Entrenamiento completado. Zenzero está agotado.");
+        }
+    }
+}
